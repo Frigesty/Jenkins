c6e787074a304736fce341eafc8db39e409cb09b
Frigesty <frigesty@gmail.com>
2023-05-13 19:23:08 +0500

    complite test

--- src/test/java/ru/frigesty/tests/TestBase.java
+++ src/test/java/ru/frigesty/tests/TestBase.java
@@ -2,6 +2,7 @@ package ru.frigesty.tests;
 
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class TestBase {
 
@@ -10,6 +11,11 @@ public class TestBase {
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
         Configuration.baseUrl = "https://demoqa.com";
-        Configuration.holdBrowserOpen = true;
+        Configuration.holdBrowserOpen = false;
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+        Configuration.browserCapabilities = capabilities;
     }
 }
